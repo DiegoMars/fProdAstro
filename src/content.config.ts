5b2a89cc47875409fcc248c71a37f0e8f37986c2
MarsCarbon <131321814+DiegoMars@users.noreply.github.com>
2025-07-11 15:09:58 -0700

    Some changes

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const opportunities = defineCollection({
-  loader: glob({ pattern: "*.md", base: "src/opportunities" }),
+  loader: glob({ pattern: "*.md", base: "src/data/opportunities" }),
   schema: z.object({
     title: z.string(),
     company: z.string(),
@@ -12,7 +12,20 @@ const opportunities = defineCollection({
     }),
     deadline: z.string(),
     link: z.string(),
-  })
+  }),
+});
+
+const farisaResearch = defineCollection({
+  loader: glob({ pattern: "*.md", base: "src/data/farisaResearch" }),
+  schema: z.object({
+    title: z.string(),
+    publicated: z.string(),
+    journal: z.string(),
+    picture: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  }),
 });
 
 export const collections = { opportunities };
